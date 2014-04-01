b05b740b2ff6d7bc7608bd20351da9c94e8efa69
Arnold van der Meulen <arnold@fmoor.nl>
2014-04-01 16:26:20 +0200

    First simple benchmark

---
+++ benchmark/test.rb
@@ -0,0 +1,21 @@
+require '../lib/palo'
+require 'benchmark'
+
+include Benchmark
+
+session = Palo::Session.new("127.0.0.1", "7777", "admin", "admin")
+
+# p session.server.databases
+# p session.sid
+url = "#{session.base_url}/server/databases?sid=#{session.sid}"
+p url
+# p `ab -n 10 -c 2 #{url}`
+
+n = 1000
+Benchmark.bm(20) do |x|
+  x.report('server/databases') do
+    n.times do
+      session.server.databases
+    end
+  end
+end
